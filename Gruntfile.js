77ab9ce0cdf1dc84d5c56ac0b44a9c27fadc6454
Rocco Janse <roccojanse@msn.com>
2014-03-04 22:53:00 +0100

    added grunt and sass

---
+++ Gruntfile.js
@@ -0,0 +1,30 @@
+module.exports = function(grunt) {
+    
+    // config
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            dist: {
+                options: {                          
+                    style: 'expanded',
+                    trace: true,
+                    precision: 3,
+                    nocache: true,
+                    banner: '<%= pkg.name %> <%= pkg.version %> (c)<%= pkg.author %>'
+                },
+                files: {
+                    'www/css/<%= pkg.name %>.css': ['src/scss/main.scss']
+                }
+            }
+        }
+
+    });
+
+    // load plugins
+    grunt.loadNpmTasks('grunt-contrib-sass');
+
+    // define task(s)
+    grunt.registerTask('default', ['sass']);
+
+};
